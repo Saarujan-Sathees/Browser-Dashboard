cd08cefec1855d73d1b5300f6bdafc1baf43f77d
Saarujan Sathees <85376227+Saarujan-Sathees@users.noreply.github.com>
2024-01-11 18:40:04 -0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,4 +1,4 @@
-// Include -lwsock32 when compiling with G++
+// g++ test.cpp -lwsock32 -std=c++17 -o dashboard-server.exe -mwindows
 #include <fstream>
 #include <iostream>
 #include <WinSock2.h>
@@ -57,27 +57,68 @@ SOCKET createSock(int type) {
     return sock;
 }
 
-void shellCommand(std::string cmd, std::string redirect) {
-    STARTUPINFOA si = {0};
-    PROCESS_INFORMATION pi = {0};
-
-    if (CreateProcessA(NULL, ("powershell " + cmd + " > " + redirect).data(), NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi)) {
-        WaitForSingleObject(pi.hProcess, INFINITE);
-        CloseHandle(pi.hProcess);
-        CloseHandle(pi.hThread);
-    } else {
-        std::cout << GetLastError() << " Shell Command\n";
-    }
-}
-
-void readCommand(std::string fileRedirect, std::string &result) {
+void readCommand(std::string fileRedirect, std::string &result, std::string start = "", std::string end = "") {
     std::ifstream input(fileRedirect);
     result = "";
     
-    while (input.good()) {
-        int curr = input.get();
-        if (curr >= 32 && curr <= 126) 
-            result += (char) curr;
+    if (start == "" && end == "") {
+        while (input.good()) {
+            int curr = input.get();
+            if (curr >= 32 && curr <= 126) 
+                result += (char) curr;
+        }
+    } else {
+        int index = 0, curr;
+        result = "";
+
+        while (input.good() && index < start.size()) { //First loop removes all the unnecessary characters
+            curr = input.get();
+            if (curr >= 32 && curr <= 126) {
+                if (start[index] == (char) curr) {
+                    result += (char) curr;
+                    ++index;
+                } else if (index > 0) {
+                    result = "";
+                    index = 0;
+                }
+            }
+        }
+
+        index = 0;
+        while (input.good() && index < end.size()) { //First loop removes all the unnecessary characters
+            curr = input.get();
+            if (curr >= 32 && curr <= 126) {
+                result += (char) curr;
+                if (end[index] == (char) curr)
+                    ++index;
+                else if (index > 0) 
+                    index = 0;
+            }
+        }
+    }
+
+    input.close();
+}
+
+void shellCommand(std::string cmd, std::string redirect, std::string start = "", std::string end = "") {
+    STARTUPINFOA si = {0};
+    PROCESS_INFORMATION pi = {0};
+    char* fullCommand = ("powershell " + cmd + " > " + redirect).data();
+    
+    if (CreateProcessA(NULL, fullCommand, NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi)) {
+        WaitForSingleObject(pi.hProcess, INFINITE);
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+        
+        if (start != "" && end != "") {
+            std::string result;
+            readCommand(redirect, result, start, end);
+            std::ofstream output(redirect);
+            output << result.c_str();
+            output.close();
+        }
+    } else {
+        std::cout << GetLastError() << " Shell Command\n";
     }
 }
 
@@ -106,26 +147,28 @@ class Server {
         return ip;
     }
 
-    static void fetchEinthusan() {
+    static void fetchInfoLoop() {
         while (1) {
-            shellCommand("(wget https://einthusan.tv/movie/browse/?lang=tamil).Content", "output.txt");
+            shellCommand("(wget https://einthusan.tv/movie/browse/?lang=tamil).Content", 
+                         "einthusan.txt", "UIFeaturedFilms", "dot-nav");
+            shellCommand("(wget https://www.google.com/search?q=weather\\\"&\\\"rlz=1C1CHBD_enCA724CA727\\\"&\\\"ie=UTF-8).ParsedHtml.body.innerHTML", 
+                         "weather.txt", "BNeawe iBp4i AP7Wnd", "<H3 class=\"zBAuLc l97dzf\">");
             std::this_thread::sleep_for(std::chrono::seconds(3600));
         }
     }
 
-    std::string movieFetch() {
+    std::string fetch(std::string fileName) {
         std::string body;
-        readCommand("output.txt", body);
-
-        log(SUCCESS, "Einthusan Fetch", __LINE__);
-        return body.substr(body.find("UIFeaturedFilms"), body.find("dot-nav"));
+        readCommand(fileName, body);
+        log(SUCCESS, fileName + " Fetch", __LINE__);
+        return body;
     }
 
     public:
     Server(const short &port) {
         wsa = startWSA();
         info.sin_family = AF_INET;
-        info.sin_addr.S_un.S_addr = inet_addr(getIPv4().c_str());
+        info.sin_addr.S_un.S_addr = INADDR_ANY;
         info.sin_port = htons(port);
 
         main = createSock(info.sin_family);
@@ -134,7 +177,7 @@ class Server {
 
     void start() {
         listen(main, 1);
-        std::thread fetch(fetchEinthusan);
+        std::thread fetchThread(fetchInfoLoop);
         while (1) {
             connection = accept(main, NULL, NULL);
             getpeername(connection, (struct sockaddr*) &address, (int*) (sizeof(address)));
@@ -145,9 +188,11 @@ class Server {
                 request = request.substr(request.find("/") + 1, request.find(" HTTP") - 5);
                 std::string response = "HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nContent-Type: text/html\r\nConnection: Closed\r\n\r\n";
                 if (request == "movie-fetch")
-                    response += movieFetch();
+                    response += fetch("einthusan.txt");
                 else if (request == "profile-information")
                     response += "";
+                else if (request == "weather-fetch")
+                    response += fetch("weather.txt");
                     
                 send(connection, response.c_str(), response.length(), 0);
             } else {
@@ -157,7 +202,7 @@ class Server {
             closesocket(connection);
         }
 
-        fetch.join();
+        fetchThread.join();
     }
 };
 
