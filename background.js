29cf65c8ee4dc464d5e2c4f111a23f3ee75f4a91
Saarujan Sathees <85376227+Saarujan-Sathees@users.noreply.github.com>
2024-01-11 18:38:18 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,8 +1,37 @@
-
+async function getOAuth(showPopup = false) {
+    if (navigator.userAgent.indexOf("Edg") != -1) {
+        return new Promise(resolve => { 
+            chrome.identity.launchWebAuthFlow({ "interactive": showPopup, "url": "https://google.com" }, token => {
+                console.log("OAuth2 Token Granted!");
+                resolve(token);
+            }); 
+        });
+    } else {
+        return new Promise(resolve => { 
+            return chrome.identity.getAuthToken({ "interactive": showPopup }, token => {
+                console.log("OAuth2 Token Granted!");
+                resolve(token);
+            }); 
+        });
+    }
+}
 
 function insertProfileData(token, tabId) {
     fetch('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + token).then(response => response.json()).then(json => {
         chrome.tabs.sendMessage(tabId, { isProfileData: true, body: json });
+    }).catch(async () => {
+        await getOAuth();
+        insertProfileData(token, tabId);
+    });;
+}
+
+async function initializeSettings() {
+    chrome.storage.local.set({ 
+        "color-scheme": "dark", 
+        "accent-color": "rgb(255 95 31)", 
+        "background-image": "no_image"
+    }).then(() => {
+        console.log("Settings set!");
     });
 }
 
@@ -14,35 +43,43 @@ function getEmail(tabId, token, messages, index) {
         chrome.tabs.sendMessage(tabId, { isProfileData: false, res: request["payload"]["headers"], 
                                          id: request["id"], isLastEmail: index == 4 });
         getEmail(tabId, token, messages, ++index);
-    });
+    }).catch(async () => {
+        await getOAuth();
+        getEmail(tabId, token, messages, index);
+    });;
 }
 
 function insertEmails(token, tabId) {
     fetch('https://www.googleapis.com/gmail/v1/users/me/messages?q=label:Forwarding&maxResults=5&alt=json&access_token=' + token)
     .then(response => response.json()).then(r => {
-        console.log(r.resultSizeEstimate);
         if (r.resultSizeEstimate == 0) {
             fetch('https://www.googleapis.com/gmail/v1/users/me/messages?maxResults=5&alt=json&access_token=' + token)
             .then(response => response.json()).then(unlabeledRes => {
                 getEmail(tabId, token, unlabeledRes["messages"], 0);
-            });
+            }).catch(async () => {
+                await getOAuth();
+                insertEmails(token, tabId);
+            });;
         } else {
             getEmail(tabId, token, r["messages"], 0);
         }
+    }).catch(async () => {
+        await getOAuth();
+        insertEmails(token, tabId);
     });
 }
 
 chrome.runtime.onInstalled.addListener(async () => {
-    await chrome.identity.getAuthToken({ interactive: true }, async (token) => {
-        console.log("OAuth2 Token Granted!");
-    });
+    await getOAuth(true);
+    let colorScheme = await chrome.storage.local.get(["color-scheme"]);
+    if (colorScheme["color-scheme"] == null)
+        initializeSettings();
 });
 
 chrome.tabs.onUpdated.addListener(async (tabId, change, tab) => {
     if (tab.url == "chrome://newtab/" && change.status == 'complete') {   
-        await chrome.identity.getAuthToken({ interactive: false }, async (token) => {
-            insertProfileData(token, tab.id);
-            insertEmails(token,tab.id);
-        });
+        let token = await getOAuth();
+        insertProfileData(token, tab.id);
+        insertEmails(token,tab.id);
     }
 });
